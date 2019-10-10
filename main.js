3f597a2fac05f9a1c9e7cfd3894c38bf03ab0fac
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2019-10-10 22:19:27 +0300

    Added style.css and main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+let loginField = document.querySelector('input[name="login"]'),
+    passwordField = document.querySelector('input[name="password"]'),
+    password2Field = document.querySelector('input[name="password2"]'),
+    submit = document.querySelector('input[name="submit"]');
+
+
+console.log(loginField);
+console.log(passwordField);
+console.log(password2Field);
+console.log(submit);
