c06c26e73d3fa1c5975b047bfd086ef69c4603c6
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2019-10-12 17:59:12 +0300

    Added html-nodes for messages in js

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 let form = document.getElementById('custom-form'),
     modal = document.getElementById('modal'),
     close = document.getElementById('close'),
+    messageBox = document.getElementById('message-box'),
     loginField = document.querySelector('input[name="login"]'),
     passwordField = document.querySelector('input[name="password"]'),
     password2Field = document.querySelector('input[name="password2"]'),
     submit = document.querySelector('input[name="submit"]'),
+    loginSuccessMessage = 'You have been successfully registered',
     loginString, passwordString, password2String, loginValidationMessage, passwordValidationMessage;
+
+/*
+* To fix errors, it's necessary to add a checking of the data received from the user
+* for inequality to null-value and undefined-value
+ */
 
 // new RegExp("");
 const loginValidationRule = /[a-z]/gi;
@@ -17,10 +24,24 @@ form.addEventListener('submit', function(event) {
     passwordString = passwordField.value;
     password2String = password2Field.value;
 
-    let loginValidationResult = loginValidation(loginString, loginValidationRule);
-    let passwordValidationResult = passwordValidation(passwordString, password2String, passwordValidationRule);
+    let loginValidationResult = loginValidation(loginString, loginValidationRule),
+        passwordValidationResult = passwordValidation(passwordString, password2String, passwordValidationRule);
 
-    
+    let loginMessageBox = document.createElement('p'),
+        passwordMessageBox = document.createElement('p'),
+        loginSuccessMessageBox = document.createElement('p');
+
+    loginMessageBox.innerHTML = loginValidationMessage;
+    passwordMessageBox.innerHTML = passwordValidationMessage;
+    loginSuccessMessageBox.innerHTML = loginSuccessMessage;
+
+    if (loginValidationResult && passwordValidationResult) {
+        messageBox.append(loginSuccessMessageBox);
+    } else if (loginValidationResult) {
+        messageBox.append(passwordMessageBox);
+    } else if (passwordValidationResult) {
+        messageBox.append(loginMessageBox);
+    }
 
     modal.style.display = "block";
 });
